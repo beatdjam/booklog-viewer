1fee6b826213dfacb0ceab69aa27506dfc6ab53c
T.Hori <beat3526@gmail.com>
2022-06-17 22:24:44 +0900

    ファイルアップロードのコンポーネントをUIの責務のみに切り出し

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {DateRange} from "./ui/date-picker/date-picker.component";
+import {parse} from "csv-parse/sync";
+import {Item} from "./ui/file-upload-form/file-upload-form.model";
+import {BooklogItemsStore} from "./state/booklog-items/booklog-items.store";
 
 @Component({
     selector: 'app-root',
@@ -8,7 +11,7 @@ import {DateRange} from "./ui/date-picker/date-picker.component";
 })
 export class AppComponent implements OnInit {
 
-    constructor() {
+    constructor(private itemStore: BooklogItemsStore) {
     }
 
     ngOnInit() {
@@ -16,6 +19,35 @@ export class AppComponent implements OnInit {
 
     dateChange($event: DateRange) {
         console.log($event);
+    }
+
+    uploadFile($event: string) {
+        this.itemStore.set(this.parseCSV($event))
+    }
+
+    private parseCSV(result: string) {
+        const records = parse(result) as string[][];
+        return records.map(record => {
+            return new Item(
+                record[0],
+                record[1],
+                record[2],
+                record[3],
+                record[4],
+                record[5],
+                record[6],
+                record[7],
+                record[8],
+                record[9],
+                record[10],
+                record[11],
+                record[12],
+                record[13],
+                record[14],
+                record[15],
+                record[16],
+            )
+        });
     }
 }
 
--- src/app/ui/file-upload-form/file-upload-form.component.ts
+++ src/app/ui/file-upload-form/file-upload-form.component.ts
@@ -1,8 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
-import {parse} from "csv-parse/sync";
-import {Item} from "./file-upload-form.model";
-import {BooklogItemsStore} from "../../state/booklog-items/booklog-items.store";
 
 @Component({
     selector: 'app-file-upload-form',
@@ -12,8 +9,9 @@ import {BooklogItemsStore} from "../../state/booklog-items/booklog-items.store";
 export class FileUploadFormComponent {
     form = this.fb.group({file: ['', [Validators.required]]});
     file: File | null = null;
+    @Output() fileString: EventEmitter<string> = new EventEmitter<string>();
 
-    constructor(private fb: FormBuilder, private itemStore: BooklogItemsStore) {
+    constructor(private fb: FormBuilder) {
     }
 
     get f() {
@@ -26,35 +24,10 @@ export class FileUploadFormComponent {
         }
     }
 
-    // TODO fileのsubmit以降の処理を剥がしてuiの責務だけにする
     submit() {
-        this.readAsText(this.file!).then(result => this.itemStore.set(this.parseCSV(result)));
+        this.readAsText(this.file!).then(result => this.fileString.emit(result)    );
     }
 
-    private parseCSV(result: string) {
-        const records = parse(result) as string[][];
-        return records.map(record => {
-            return new Item(
-                record[0],
-                record[1],
-                record[2],
-                record[3],
-                record[4],
-                record[5],
-                record[6],
-                record[7],
-                record[8],
-                record[9],
-                record[10],
-                record[11],
-                record[12],
-                record[13],
-                record[14],
-                record[15],
-                record[16],
-            )
-        });
-    }
 
     // アップロードされたファイルを読み込み、テキストとして返す
     private readAsText(file: File): Promise<string> {
