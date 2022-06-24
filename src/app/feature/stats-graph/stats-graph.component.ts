fc6befb0e964b15f3574d6679a2c382eca73f500
T.Hori <beat3526@gmail.com>
2022-06-25 01:04:03 +0900

    日付の変更を検知してグラフを変えるよう変更した

--- src/app/feature/stats-graph/stats-graph.component.ts
+++ src/app/feature/stats-graph/stats-graph.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, SimpleChanges} from '@angular/core';
-import {filter, map, Subject, combineLatest} from "rxjs";
+import {combineLatest, map, Subject} from "rxjs";
 import {BooklogItemsQuery} from "../../state/booklog-items/booklog-items.query";
 import {StatusGraphViewModel} from "./status-graph.viewmodel";
 import {DateRange} from "../../ui/date-picker/date-picker.component";
@@ -10,28 +10,21 @@ import {DateRange} from "../../ui/date-picker/date-picker.component";
     styleUrls: ['./stats-graph.component.scss']
 })
 export class StatsGraphComponent {
-    state$ = this.booklogItemsQuery.selectAll()
+    private dateRangeSubject = new Subject<DateRange | null>();
+    state$ = combineLatest([this.booklogItemsQuery.selectAll(), this.dateRangeSubject.asObservable()])
         .pipe(
-            filter(items => items.length > 0),
-            map(items => {
-                if (this.dateRange === null) {
-                    return items;
-                } else {
-                    return items.filter(item => this.dateRange!.start <= item.createAt && item.createAt <= this.dateRange!.end );
-                }
-            }),
+            map(([items, dateRange]) => items.filter(item => dateRange === null || (dateRange.start <= item.createAt && item.createAt <= dateRange.end))),
             map(items => new StatusGraphViewModel(items))
-        );
-
+        )
     @Input() dateRange: DateRange | null = null;
 
     constructor(private booklogItemsQuery: BooklogItemsQuery) {
+        this.dateRangeSubject.next(null);
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        // TODO 検知した変更をstateに流す
         const dateRange = changes['dateRange'].currentValue as DateRange | null;
-        console.log(dateRange);
+        if (!!dateRange) this.dateRangeSubject.next(dateRange);
     }
 }
 
